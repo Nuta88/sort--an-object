09bb54bd4cfc24d0fd7a45130aae1467dbddca34
nuta88 <anna.vagner-88@mail.ru>
2017-07-10 10:34:18 +0300

    Added tests and delete some code with sortObj.js.

--- sortObj.js
+++ sortObj.js
@@ -1,28 +1,18 @@
 "use strict";
 function Car(year, carBrand) {
 	this.year = year;
-	this.carBrand = carBrand; 
+	this.carBrand = carBrand;
 }
 
-var audi = new Car(2010, "audi");
-var bmw = new Car(1901, "bmw");
-var lexus = new Car(2010, "lexus");
-var lexusTwo = new Car(2010, "lexus");
-var toyota = new Car(2009, "toyota");
-var nisssan = new Car(2015, "nisssan");
-var arr = [ bmw, audi, lexus, toyota, nisssan, lexusTwo ];
-
-function compare(objectOne, ObjectTwo) {
-	if (objectOne.year !== ObjectTwo.year) {
-		return objectOne.year - ObjectTwo.year;
+function compare(car1, car2) {
+	if (car1.year !== car2.year) {
+		return car1.year - car2.year;
 	}
-	if (objectOne.carBrand > ObjectTwo.carBrand) {
+	if (car1.carBrand > car2.carBrand) {
 		return 1;
 	}
-	if (objectOne.carBrand < ObjectTwo.carBrand) {
+	if (car1.carBrand < car2.carBrand) {
 		return -1;
 	}
-		return 0;
+	return 0;
 }
-
-arr.sort(compare);
--- test.js
+++ test.js
@@ -1,20 +1,48 @@
+QUnit.test("added objects with constractor", function(assert) {
+	var audi = new Car(2010, "audi");
+	var bmw = new Car(1901, "bmw");
+	var lexus = new Car(2010, "lexus");
+	var lexusTwo = new Car(2010, "lexus");
+	var toyota = new Car(2009, "toyota");
+	var nisssan = new Car(2015, "nisssan");
+	var input = [ bmw, audi, lexus, toyota, nisssan, lexusTwo ];
+	var expected = [ bmw, audi, lexus, toyota, nisssan, lexusTwo ];
+	assert.deepEqual(input, expected, "Two arrays must be the same in value");
+});
+
 QUnit.test("function compare a>b test", function(assert) {
+	var audi = new Car(2010, "audi");
+	var bmw = new Car(1901, "bmw");
 	var actual = compare(audi, bmw);
 	var expected = 109;
-	assert.deepEqual(actual, expected,
-			"Return positive number");
+	assert.deepEqual(actual, expected, "Return positive number");
 });
 
 QUnit.test("function compare a<b test", function(assert) {
+	var nisssan = new Car(2015, "nisssan");
+	var bmw = new Car(1901, "bmw");
 	var actual = compare(bmw, nisssan);
 	var expected = -114;
-	assert.deepEqual(actual, expected,
-			"Return negative number");
+	assert.deepEqual(actual, expected, "Return negative number");
 });
 
 QUnit.test("function compare a===b test", function(assert) {
+	var lexus = new Car(2010, "lexus");
+	var lexusTwo = new Car(2010, "lexus");
 	var actual = compare(lexus, lexusTwo);
 	var expected = 0;
-	assert.deepEqual(actual, expected,
-			"Return zero");
+	assert.deepEqual(actual, expected, "Return zero");
+});
+
+QUnit.test("sort method test", function(assert) {
+	var audi = new Car(2010, "audi");
+	var bmw = new Car(1901, "bmw");
+	var lexus = new Car(2010, "lexus");
+	var lexusTwo = new Car(2010, "lexus");
+	var toyota = new Car(2009, "toyota");
+	var nisssan = new Car(2015, "nisssan");
+	var input = [ bmw, audi, lexus, toyota, nisssan, lexusTwo ];
+	var actual = input.sort(compare);
+	var expected = [ bmw, toyota, audi, lexus, lexusTwo, nisssan ];
+	assert.deepEqual(actual, expected, "Return positive number");
 });
